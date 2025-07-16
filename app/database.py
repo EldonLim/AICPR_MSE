20eb52ce3d34358113bd27e7dc811658fdddd15a
Eldon Lim Kai Jie <82358428+EldonLim@users.noreply.github.com>
2025-07-16 11:00:04 +0800

    Delete app directory

--- app/database.py
+++
@@ -1,9 +0,0 @@
-# app/database.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://myuser:mypassword@db:5432/mydb"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-Base = declarative_base()
